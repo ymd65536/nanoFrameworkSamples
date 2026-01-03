a8a8e2901b96a50c9a2a7246de891801c03d9984
ymd65536 <ymd65536@gmail.com>
2026-01-03 13:08:20 +0900

    Add LED Bar example project files and configuration

---
+++ OnBoard/Examples/05_LED_Bar/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("LED_Bar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LED_Bar")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
